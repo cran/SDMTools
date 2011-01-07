c58fc8e1752c0b080534043ceeafcc381e38ebbf
Jeremy VanDerWal <jjvanderwal@gmail.com>
2011-01-07 00:00:00 +0000

    version 1.1-5

--- R/put.data.R
+++ R/put.data.R
@@ -1,9 +1,7 @@
-extract.data <-
+put.data <-
 function(pts, x) {
-	#check if raster from sp or raster package and convert if necessary
-	if (any(class(x) %in% 'RasterLayer')) x = asc.from.raster(x)
-	if (any(class(x) == 'SpatialGridDataFrame')) x = asc.from.sp(x)
 	if (class(x) != 'asc') stop('matrix must be of class "asc"') #check to ensure x is of class asc
+	pts = as.matrix(pts); if (dim(pts)[2]!=3) stop('input pts must be 3 columns of data')
 	xy <- getXYcoords(x)
 	xy$x <- xy$x + attr(x, "cellsize")/2
 	xy$x <- c(xy$x[1] - attr(x, "cellsize"),xy$x)
@@ -11,5 +9,6 @@ function(pts, x) {
 	xy$y <- c(xy$y[1] - attr(x, "cellsize"),xy$y)
 	xf <- as.numeric(cut(pts[, 1], xy$x))
 	yf <- as.numeric(cut(pts[, 2], xy$y))
-	return(x[cbind(xf,yf)])
+	x[cbind(xf,yf)] = pts[,3]
+	return(x)
 }
